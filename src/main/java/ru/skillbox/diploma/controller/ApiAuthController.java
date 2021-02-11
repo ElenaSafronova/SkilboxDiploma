1e503e6e0b11a996a06fd8d93e4635081813facc
Elena Safronova <elenasafronova1987@gmail.com>
2021-02-11 16:34:58 +0300

    AuthService

--- src/main/java/ru/skillbox/diploma/controller/ApiAuthController.java
+++ src/main/java/ru/skillbox/diploma/controller/ApiAuthController.java
@@ -5,32 +5,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.skillbox.diploma.Dto.*;
 import ru.skillbox.diploma.exception.EmailExistsException;
-import ru.skillbox.diploma.model.User;
+import ru.skillbox.diploma.service.AuthService;
 import ru.skillbox.diploma.service.CaptchaService;
 import ru.skillbox.diploma.service.PostService;
 import ru.skillbox.diploma.service.UserService;
-import ru.skillbox.diploma.value.PostStatus;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/auth")
 public class ApiAuthController {
+    Logger logger = LoggerFactory.getLogger(ApiAuthController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthService authService;
 
     @Autowired
     private CaptchaService captchaService;
 
     @Autowired
     private PostService postService;
-
-    Logger logger = LoggerFactory.getLogger(ApiAuthController.class);
 
     @GetMapping("/check")
     public ResponseEntity<String> checkAuthorization(Model model){
@@ -56,31 +57,7 @@ public class ApiAuthController {
     public ResponseEntity<AuthenticationDto> authUser(@RequestBody LoginDto loginDto) {
         logger.trace("POST /api/auth/login");
         logger.trace("loginDto = " + loginDto.toString());
-
-        if (loginDto == null){
-            return new ResponseEntity<>(null, HttpStatus.OK);
-        }
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String curEmail = loginDto.getE_mail();
-        String curPass = loginDto.getPassword();
-
-        User actualUser = userService.findUserByEmail(curEmail);
-
-        if (actualUser == null){
-            logger.info("user " + curEmail + " not found");
-            return new ResponseEntity<>(new AuthenticationDto(null), HttpStatus.OK);
-        }
-
-        if (!encoder.matches(curPass, actualUser.getPassword())){
-            logger.info(curEmail + " password is wrong. " + curPass);
-            return new ResponseEntity<>(new AuthenticationDto(null), HttpStatus.OK);
-        }
-        UserDataAuthDto userDataAuthDto = new UserDataAuthDto(actualUser);
-        if (userDataAuthDto.getModerationCount() > 0){
-            logger.trace("postService.countByModerationStatus(PostStatus.NEW)");
-            userDataAuthDto.setModerationCount(postService.countByModerationStatus(PostStatus.NEW));
-        }
+        UserDataAuthDto userDataAuthDto = authService.login(loginDto);
 
         return new ResponseEntity<>(new AuthenticationDto(userDataAuthDto), HttpStatus.OK);
     }
---
+++ src/main/java/ru/skillbox/diploma/service/AuthService.java
@@ -0,0 +1,50 @@
+package ru.skillbox.diploma.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.skillbox.diploma.Dto.LoginDto;
+import ru.skillbox.diploma.Dto.UserDataAuthDto;
+import ru.skillbox.diploma.model.User;
+import ru.skillbox.diploma.value.PostStatus;
+
+@Service
+public class AuthService {
+    Logger logger = LoggerFactory.getLogger(AuthService.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PostService postService;
+
+    public UserDataAuthDto login(LoginDto loginDto) {
+        if (loginDto == null){
+            return null;
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String curEmail = loginDto.getE_mail();
+        String curPass = loginDto.getPassword();
+
+        User actualUser = userService.findUserByEmail(curEmail);
+
+        if (actualUser == null){
+            logger.info("user " + curEmail + " not found");
+            return null;
+        }
+
+        if (!encoder.matches(curPass, actualUser.getPassword())){
+            logger.info(curEmail + " password is wrong. " + curPass);
+            return null;
+        }
+        UserDataAuthDto userDataAuthDto = new UserDataAuthDto(actualUser);
+        if (userDataAuthDto.getModerationCount() > 0){
+            logger.trace("postService.countByModerationStatus(PostStatus.NEW)");
+            userDataAuthDto.setModerationCount(postService.countByModerationStatus(PostStatus.NEW));
+        }
+        return userDataAuthDto;
+    }
+}
